8642fcef341209a56d9da8469f56e196cd181bdd
codersguild <shekharhiran@hotmail.com>
2020-03-25 22:39:12 +0530

    Deployed Network

---
+++ test-net/commands.sh
@@ -0,0 +1,14 @@
+## First Command needs to be --isInit . It has already been done by the scripts executed before.
+## We use majority rule so endoersing by two peers are enough
+
+peer chaincode invoke -o orderer.payzchain.com:7050 --tls true --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/payzchain.com/orderers/orderer.payzchain.com/msp/tlscacerts/tlsca.payzchain.com-cert.pem -C payzchannel -n marblescc --peerAddresses peer0.org1.payzchain.com:7051 --tlsRootCertFiles /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.payzchain.com/peers/peer0.org1.payzchain.com/tls/ca.crt --peerAddresses peer0.org2.payzchain.com:9051 --tlsRootCertFiles /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org2.payzchain.com/peers/peer0.org2.payzchain.com/tls/ca.crt -c '{"Args":["initMarble","marble1","blue","35","tom"]}'
+
+peer chaincode invoke -o orderer.payzchain.com:7050 --tls true --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/payzchain.com/orderers/orderer.payzchain.com/msp/tlscacerts/tlsca.payzchain.com-cert.pem -C payzchannel -n marblescc --peerAddresses peer0.org1.payzchain.com:7051 --tlsRootCertFiles /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.payzchain.com/peers/peer0.org1.payzchain.com/tls/ca.crt --peerAddresses peer0.org2.payzchain.com:9051 --tlsRootCertFiles /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org2.payzchain.com/peers/peer0.org2.payzchain.com/tls/ca.crt -c '{"Args":["initMarble","marble2","red","50","tom"]}'
+
+peer chaincode invoke -o orderer.payzchain.com:7050 --tls true --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/payzchain.com/orderers/orderer.payzchain.com/msp/tlscacerts/tlsca.payzchain.com-cert.pem -C payzchannel -n marblescc --peerAddresses peer0.org1.payzchain.com:7051 --tlsRootCertFiles /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.payzchain.com/peers/peer0.org1.payzchain.com/tls/ca.crt --peerAddresses peer0.org2.payzchain.com:9051 --tlsRootCertFiles /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org2.payzchain.com/peers/peer0.org2.payzchain.com/tls/ca.crt -c '{"Args":["initMarble","marble3","blue","70","tom"]}'
+
+peer chaincode query -C payzchannel -n marblescc -c '{"Args":["getHistoryForMarble","marble1"]}'
+peer chaincode query -C payzchannel -n marblescc -c '{"Args":["queryMarbles","{\"selector\":{\"owner\":\"tom\"}}"]}'
+peer chaincode query -C payzchannel -n marblescc -c '{"Args":["queryMarblesWithPagination","{\"selector\":{\"owner\":\"tom\"}}","3",""]}'
+
+peer chaincode query -C payzchannel -n marblescc -c '{"Args":["getMarblesByRange","marble1","marble3"]}'
---
+++ test-net/scripts/testorg3.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+#
+# Copyright IBM Corp. All Rights Reserved.
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# This script is designed to be run in the org3cli container as the
+# final step of the EYFN tutorial. It simply issues a couple of
+# chaincode requests through the org3 peers to check that org3 was
+# properly added to the network previously setup in the BYFN tutorial.
+#
+
+echo
+echo " ____    _____      _      ____    _____ "
+echo "/ ___|  |_   _|    / \    |  _ \  |_   _|"
+echo "\___ \    | |     / _ \   | |_) |   | |  "
+echo " ___) |   | |    / ___ \  |  _ <    | |  "
+echo "|____/    |_|   /_/   \_\ |_| \_\   |_|  "
+echo
+echo "Extend your first network (EYFN) test"
+echo
+CHANNEL_NAME="$1"
+DELAY="$2"
+CC_SRC_LANGUAGE="$3"
+TIMEOUT="$4"
+VERBOSE="$5"
+: ${CHANNEL_NAME:="payzchannel"}
+: ${TIMEOUT:="10"}
+: ${CC_SRC_LANGUAGE:="go"}
+: ${VERBOSE:="false"}
+CC_SRC_LANGUAGE=`echo "$CC_SRC_LANGUAGE" | tr [:upper:] [:lower:]`
+COUNTER=1
+MAX_RETRY=5
+
+if [ "$CC_SRC_LANGUAGE" = "go" -o "$CC_SRC_LANGUAGE" = "golang" ]; then
+	CC_RUNTIME_LANGUAGE=golang
+	CC_SRC_PATH="github.com/hyperledger/fabric-samples/chaincode/marbles02/go/"
+elif [ "$CC_SRC_LANGUAGE" = "javascript" ]; then
+	CC_RUNTIME_LANGUAGE=node # chaincode runtime language is node.js
+	CC_SRC_PATH="/opt/gopath/src/github.com/hyperledger/fabric-samples/chaincode/marbles02/javascript/"
+elif [ "$CC_SRC_LANGUAGE" = "java" ]; then
+	CC_RUNTIME_LANGUAGE=java
+	CC_SRC_PATH="/opt/gopath/src/github.com/hyperledger/fabric-samples/chaincode/marbles02/java/"
+else
+	echo The chaincode language ${CC_SRC_LANGUAGE} is not supported by this script
+	echo Supported chaincode languages are: go, javascript, java
+	exit 1
+fi
+
+echo "Channel name : "$CHANNEL_NAME
+
+# import functions
+. scripts/utils.sh
+
+
+echo
+echo "========= All GOOD, EYFN test execution completed =========== "
+echo
+
+echo
+echo " _____   _   _   ____   "
+echo "| ____| | \ | | |  _ \  "
+echo "|  _|   |  \| | | | | | "
+echo "| |___  | |\  | | |_| | "
+echo "|_____| |_| \_| |____/  "
+echo
+
+exit 0
