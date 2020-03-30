561e011357c8d438d9aaf598317fd4bbdeace7f9
codersguild <shekharhiran@hotmail.com>
2020-03-31 01:26:48 +0530

    invoke

---
+++ test-net/scripts/assemble.sh
@@ -0,0 +1,7 @@
+file="./dump.in"
+commands=`cat $file`
+for command in $commands; do
+	echo peer chaincode invoke -o orderer.payzchain.com:7050 --tls true --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/payzchain.com/orderers/orderer.payzchain.com/msp/tlscacerts/tlsca.payzchain.com-cert.pem -C payzchannel -n payzcc --peerAddresses peer0.org1.payzchain.com:7051 --tlsRootCertFiles /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.payzchain.com/peers/peer0.org1.payzchain.com/tls/ca.crt --peerAddresses peer0.org2.payzchain.com:9051 --tlsRootCertFiles /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org2.payzchain.com/peers/peer0.org2.payzchain.com/tls/ca.crt -c  \'$command\'
+	echo
+	echo
+done
